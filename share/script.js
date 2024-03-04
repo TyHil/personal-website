945ea9d27c4a000484660b99108b95d730b58286
Tyler Hill <gotyhill@gmail.com>
2024-03-04 00:50:47 -0600

    Format

--- share/script.js
+++ share/script.js
@@ -1,23 +1,23 @@
-const qrcodeDiv = document.getElementById("qrcode");
+const qrcodeDiv = document.getElementById('qrcode');
 function setQRCode(link) {
-  while(qrcodeDiv.firstChild) {
+  while (qrcodeDiv.firstChild) {
     qrcodeDiv.removeChild(qrcodeDiv.firstChild);
   }
   new QRCode(qrcodeDiv, {
     text: link,
     width: 512,
     height: 512,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
-    correctLevel : QRCode.CorrectLevel.H
+    colorDark: '#000000',
+    colorLight: '#ffffff',
+    correctLevel: QRCode.CorrectLevel.H
   });
 }
-const defaultHref = "https://tylergordonhill.com";
+const defaultHref = 'https://tylergordonhill.com';
 setQRCode(defaultHref);
 
 const buttons = document.getElementsByTagName('button');
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function() {
+  buttons[i].addEventListener('click', function () {
     if (this.classList.contains('selected')) {
       this.classList.remove('selected');
       setQRCode(defaultHref);
