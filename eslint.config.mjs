b880d01be372179c2ff86f242b2ca44f9eebba4d
Tyler Hill <gotyhill@gmail.com>
2024-11-20 23:55:02 -0600

    Format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,35 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
-export default [{
+export default [
+  {
     ignores: [
-        "**/.cache",
-        "**/.config",
-        "**/.local",
-        "src/set-operations-for-spotify",
-        "src/typewriter-formatting",
-        "src/cah-maker",
-        "src/response-display",
-        "src/password-generator",
-        "src/recipes",
-        "src/dvd",
-    ],
-}, ...compat.extends("prettier"), {
+      '**/.cache',
+      '**/.config',
+      '**/.local',
+      'src/set-operations-for-spotify',
+      'src/typewriter-formatting',
+      'src/cah-maker',
+      'src/response-display',
+      'src/password-generator',
+      'src/recipes',
+      'src/dvd'
+    ]
+  },
+  ...compat.extends('prettier'),
+  {
     languageOptions: {
-        globals: {},
-    },
-}];
+      globals: {}
+    }
+  }
+];
