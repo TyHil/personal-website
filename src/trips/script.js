c42b4274a5d9a8c702163a96f5bd256cd57be2b2
Tyler Hill <gotyhill@gmail.com>
2026-04-23 00:43:36 -0500

    Lines working on map

--- src/trips/script.js
+++ src/trips/script.js
@@ -9,10 +9,35 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matc
 const map = new jsVectorMap({
   map: 'world',
   selector: document.getElementById('map'),
-  showTooltip: false,
+  onRegionTooltipShow: function (event, tooltip, code) {
+    event.preventDefault();
+  },
+  zoomButtons: false,
+  regionStyle: {
+    initial: {
+      fill: 'var(--light1)'
+    }
+  },
   onLoaded(map) {
     window.addEventListener('resize', () => {
       map.updateSize();
     });
-  }
+  },
+  markerStyle: {
+    initial: { stroke: 'transparent', fill: 'var(--accent)', r: 5 }
+  },
+  lineStyle: {
+    stroke: 'var(--accent)',
+    strokeWidth: 3,
+    strokeDasharray: '6 6',
+    curvature: 0.2
+  },
+  markers: [
+    { name: 'Austin', coords: [30.27, -97.74] },
+    { name: 'Los Angeles', coords: [34.05, -118.24] },
+  ],
+  lines: [
+    { from: 'Los Angeles', to: 'Austin' },
+    { from: 'Austin', to: 'Los Angeles' },
+  ]
 });
